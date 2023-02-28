05f52fa6ca587dbd202c0e7c4be248542defb70e
Roberto Filho <roberto.siilva@hotmail.com>
2023-02-28 15:23:08 -0300

    Initial Commit

---
+++ Mycar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mycar
+//
+//  Created by Roberto Filho on 28/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mycar/MycarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MycarApp.swift
+//  Mycar
+//
+//  Created by Roberto Filho on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MycarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
